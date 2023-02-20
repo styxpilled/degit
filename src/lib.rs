5e82d740f999deedde19c6167b0c6f225f71f99f
styxpilled <styxpilled.dev@gmail.com>
2023-02-20 16:05:17 +0100

    feat: match with regex

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,84 @@
 use flate2::read::GzDecoder;
 use indicatif::{ProgressBar, ProgressStyle};
+use regex::Regex;
 use reqwest::blocking;
 use std::{error::Error, path::PathBuf};
 use tar::Archive;
 
-pub fn degit(src: &str) {
-    download(src).unwrap();
+#[derive(Debug, Clone)]
+pub struct Repository {
+    host: String,
+    name: String,
+    owner: String,
+    src: Option<String>,
 }
 
-pub fn download(src: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let dest = PathBuf::from("./test/");
+pub enum Host {
+    Github,
+    Gitlab(String),
+    Custom(String),
+}
+
+pub fn degit(src: &str) {
+    // download(src).unwrap();
+    let repo = dbg!(parse(src)).unwrap().unwrap();
+    let name = repo.name.clone();
+    download(repo, PathBuf::from(format!("./{}", name))).unwrap();
+}
+
+fn parse(src: &str) -> Result<Option<Repository>, Box<dyn Error>> {
+    let longhand = Regex::new(
+        r"(?x)
+    (?P<protocol>(git@|https://))
+    (?P<host>([\w\.@]+))
+    (/|:)
+    (?P<owner>[\w,\-,_]+)/(?P<name>[\w,\-,_]+)(.git)?/?",
+    )?;
+    if let Some(captures) = longhand.captures(src) {
+        return Ok(Some(Repository {
+            owner: captures.name("owner").unwrap().as_str().to_string(),
+            name: captures.name("name").unwrap().as_str().to_string(),
+            host: captures.name("host").unwrap().as_str().to_string(),
+            src: Some(captures.get(0).unwrap().as_str().to_string()),
+        }));
+    }
+    let shorthand = Regex::new(
+        r"(?x)(?P<host>(github|gitlab|bitbucket)?)
+            (?P<colon>(:))?
+            (?P<owner>[\w,\-,_]+)/(?P<name>[\w,\-,_]+)",
+    )?;
+    if let Some(captures) = shorthand.captures(src) {
+        return Ok(Some(Repository {
+            owner: captures.name("owner").unwrap().as_str().to_string(),
+            name: captures.name("name").unwrap().as_str().to_string(),
+            host: match captures.name("host") {
+                Some(host) => match host.as_str() {
+                    "" => "github".to_string(),
+                    _ => host.as_str().to_string(),
+                },
+                None => "github".to_string(),
+            },
+            src: None,
+        }));
+    }
+
+    Ok(None)
+}
+
+pub fn download(repo: Repository, dest: PathBuf) -> Result<(), Box<dyn std::error::Error>> {
+    let src = if repo.src.is_some() {
+        repo.src.unwrap()
+    } else {
+        dbg!("https://{}/{}/{}", &repo.host, &repo.owner, &repo.name);
+        match repo.host.as_str() {
+            "github" => format!(
+                "https://github.com/{}/{}/archive/HEAD.tar.gz",
+                repo.owner, repo.name
+            ),
+            _ => format!("https://{}/{}/{}", repo.host, repo.owner, repo.name),
+        }
+    };
     let client = blocking::Client::new();
-    // let url = "https://github.com/styxpilled/termdraw/archive/HEAD.tar.gz";
-    // let url = "https://github.com/seanmonstar/reqwest/archive/HEAD.tar.gz";
     let response = client.get(src).send()?;
     let total_size = response.content_length();
 
